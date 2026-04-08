099ae5c55bef2fbe7cdcf1fbde81bb0fb4e78b99
priyanshucloud02 <priyanshucloud02@gmail.com>
2026-04-08 10:15:42 +0530

    Update Main.java

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -1,25 +1,36 @@
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Read n integers. For each number print:
-        //   "Prime"     if it is prime
-        //   "Perfect"   if it is a perfect number (sum of proper divisors == itself, e.g. 6=1+2+3)
-        //   "Both"      if it is both (there are none < 100, but handle it)
-        //   "Neither"   otherwise
-        //
-        // Input:
-        // 4
-        // 6 13 8 28
-        //
-        // Output:
-        // Perfect
-        // Prime
-        // Neither
-        // Perfect
+        int[][] m = new int[n][n];
+        int top = 0, bottom = n-1, left = 0, right = n-1, num = 1;
 
+        while (num <= n * n) {
+            for (int i = left; i <= right; i++)  m[top][i]    = num++;
+            top++;
+            for (int i = top; i <= bottom; i++)  m[i][right]  = num++;
+            right--;
+            for (int i = right; i >= left; i--)  m[bottom][i] = num++;
+            bottom--;
+            for (int i = bottom; i >= top; i--)  m[i][left]   = num++;
+            left++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                if (j > 0) sb.append(" ");
+                sb.append(m[i][j]);
+            }
+            System.out.println(sb);
+        }
+
+        int diag = 0;
+        for (int i = 0; i < n; i++) diag += m[i][i];
+        System.out.println("Diagonal: " + diag);
     }
 }
